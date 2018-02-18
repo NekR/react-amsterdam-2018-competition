28273323ea01496f3d39965a4ee745c239616810
Alexander Kurganow <me@akurganow.ru>
2018-02-18 17:04:59 +0300

    Add components

---
+++ src/components/Body/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './index.css'
+
+export default () => (
+  <div className="Body-body">
+    <p>
+      Quisque molestie lorem euismod risus blandit, quis bibendum nulla rutrum. Donec sit amet consequat justo, a
+      elementum tortor. Phasellus hendrerit ut ex eu accumsan. Maecenas ullamcorper cursus enim, id pretium est luctus
+      eget. Suspendisse vulputate elementum ex sed cursus. In purus magna, tempor non nisl vel, aliquet tempor sem.
+      Curabitur odio augue, tincidunt sit amet orci at, porta porta orci. Fusce vestibulum euismod nisl mollis pretium.
+      Mauris dapibus, libero ac vulputate aliquam, nulla tellus congue urna, non hendrerit odio mauris eget neque.
+    </p>
+  </div>
+)
---
+++ src/components/Footer/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import './index.css'
+
+export default () => (
+  <footer className="Footer-footer">
+    <a href="//spb-frontend.ru">SPB Frontend 2018</a>
+  </footer>
+)
